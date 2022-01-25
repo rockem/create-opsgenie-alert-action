1f6c2387f1abba937e534537a3635f5a6e002510
Eli Segal <eli.segal@gmail.com>
2022-01-25 17:09:48 +0200

    Implemented v1

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# create-opsgenie-alert-action
+# Create OpsGenie alert action
+
+This action creates a new OpsGenie alert
+
+## Inputs
+
+`api_key`
+
+[**Required**] The api key provided by OpsGenie integration.
+
+`alias`
+
+[**Required**] The alias for the alert.
+
+`message`
+
+[**Required**] The actual alert message.
+
+`priority`
+
+[**Required**] Alert's priority, valid values: P1-P5.
+
+`description`
+
+The description for the new alert.
+
+`tags`
+
+The tags for the new alert, separated by commas. 
+
+## Example usage
+```
+uses: rockem/create-opsgenie-alert-action@v1
+with:
+    api_key: ${{ secrets.OPSGENIE_API_KEY }}
+    alias: timeout-alert-alias
+    message: Activity exceeded timeout: exceeded 60 seconds
+    priority: P2
+    tags: backend,timeout
+``` 
---
+++ index.js
@@ -0,0 +1,32 @@
+const core = require('@actions/core');
+const opsgenie = require('opsgenie-sdk');
+
+opsgenie.configure({
+    'api_key': core.getInput('api_key')
+});
+
+const inputTags = () => {
+    const tags = core.getInput('tags');
+    return !tags ? [] : tags.split(',').map(tag => {
+        return tag.trim()
+    })
+}
+
+const create_alert_request = {
+    message: core.getInput('message'),
+    alias: core.getInput('alias'),
+    description: core.getInput('description'),
+    priority: core.getInput('priority'),
+    tags: inputTags()
+}
+
+console.log(`Creating alert with: ${create_alert_request}`)
+
+opsgenie.alertV2.create(create_alert_request, function (error, alert) {
+    if (error) {
+        core.setFailed(`ERROR: ${error.message}`);
+    } else {
+        console.log(`Request sent for creating new alert: ${create_alert_request.message}`);
+    }
+});
+
