b419be85b60629202a00ff9841e59b636e2bbc82
Eli Segal <eli.segal@gmail.com>
2022-01-25 16:58:18 +0200

    Fixed messages

--- index.js
+++ index.js
@@ -20,11 +20,13 @@ const create_alert_request = {
     tags: inputTags()
 }
 
+console.log(`Creating alert with: ${create_alert_request}`)
+
 opsgenie.alertV2.create(create_alert_request, function (error, alert) {
     if (error) {
         core.setFailed(`ERROR: ${error.message}`);
     } else {
-        console.log(`Request sent for creating alert with alias: ${core.getInput('alias')}`);
+        console.log(`Request sent for creating new alert: ${create_alert_request.message}`);
     }
 });
 
