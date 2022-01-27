3d8367329784bd3b225d5928ec873acb49489f90
Debora <debora.piu@cazoo.co.uk>
2022-01-27 10:10:11 +0000

    boolean eu api endpoint

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const core = require('@actions/core');
 const opsgenie = require('opsgenie-sdk');
 
-opsgenie.configure({
-    'api_key': core.getInput('api_key'),
-    'host': core.getInput('opsgenie_api_endpoint'),
-});
+
+if (core.getInput('opsgenie_eu')) {
+    opsgenie.configure({
+        'api_key': core.getInput('api_key'),
+        'host': 'https://api.eu.opsgenie.com',
+    });
+} else {
+    opsgenie.configure({
+        'api_key': core.getInput('api_key'),
+    });
+}
+
 
 const inputTags = () => {
     const tags = core.getInput('tags');
