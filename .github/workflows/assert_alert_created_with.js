cf50509b727b2c1d7a6550501107c7eed35d2012
Eli Segal <eli.segal@gmail.com>
2022-02-09 22:30:20 +0200

    wip

---
+++ .github/workflows/assert_alert_created_with.js
@@ -0,0 +1,29 @@
+#!/bin/sh node
+
+const opsgenie = require('opsgenie-sdk');
+const expect = require('chai').expect
+
+const alias = process.argv[2];
+const message = process.argv[3];
+const priority = process.argv[4];
+const tags = process.argv[5];
+
+
+opsgenie.configure({
+    api_key: process.env.OPSGENIE_API_KEY
+})
+
+const get_alert_identifier = {
+    identifier: alias,
+    identifierType: "alias"
+};
+
+opsgenie.alertV2.get(get_alert_identifier, function (error, alert) {
+    if (error) {
+        console.log(`ERROR: ${error}`);
+    } else {
+        expect(message).to.equal(alert.data.message);
+        expect(priority).to.equal(alert.data.priority);
+
+    }
+});
