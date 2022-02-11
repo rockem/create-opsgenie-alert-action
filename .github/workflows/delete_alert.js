f5fc7d66c7f2cd2cda82f17a584dd79e77391368
Eli Segal <eli.segal@gmail.com>
2022-02-11 15:46:08 +0200

    Delete alert after test

---
+++ .github/workflows/delete_alert.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const opsgenie = require('opsgenie-sdk');
+
+const alias = process.argv[2];
+const opsgenie_api_key = process.env.OPSGENIE_API_KEY
+
+if(alias === undefined) {
+    console.log('usage: ./delete_alert.js alias')
+    process.exit(1)
+}
+
+if(opsgenie_api_key === undefined) {
+    console.log('Environment variable "OPSGENIE_API_KEY" is undefined')
+    process.exit(1)
+}
+
+opsgenie.configure({
+    api_key: process.env.OPSGENIE_API_KEY
+})
+
+const alert_identifier = {
+    identifier: alias,
+    identifierType: "alias"
+};
+
+opsgenie.alertV2.delete(alert_identifier, function (error, alert) {
+    if (error) {
+        throw new Error(error.message);
+    } else {
+        console.log(alert)
+        console.log(`Sent request to delete alert with alias: ${alias}`)
+    }
+});
