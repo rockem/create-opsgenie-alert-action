141cddc66f996dc2e78174c9b910122f3a4d5503
Eli Segal <eli.segal@gmail.com>
2022-02-12 16:34:53 +0200

    wip

--- spec/create-alert.spec.js
+++ spec/create-alert.spec.js
@@ -1,16 +1,16 @@
 const {RunOptions, RunTarget} = require('github-action-ts-run-api');
 
-const target = RunTarget.mainJsScript('action.yml');
-
-describe('Create Alert', function() {
-    it('define list of tags', async () => {
-        const options = RunOptions.create().setInputs({
-            api_key: process.env.OPSGENIE_API_KEY,
-            alias: 'alert-alias',
-
-        })
-
-
-        const res = await target.run(options);
-    });
-});
+// const target = RunTarget.mainJsScript('action.yml');
+//
+// describe('Create Alert', function() {
+//     it('define list of tags', async () => {
+//         const options = RunOptions.create().setInputs({
+//             api_key: process.env.OPSGENIE_API_KEY,
+//             alias: 'alert-alias',
+//
+//         })
+//
+//
+//         const res = await target.run(options);
+//     });
+// });
