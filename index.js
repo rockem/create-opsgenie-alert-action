b362e21306fbfc840cc43ffe5d6a5e5aa010aedd
Eli Segal <eli.segal@gmail.com>
2022-02-18 19:41:28 +0200

    wip

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const allInputs = () => {
     const inputs = {}
     for (let [k,v] of Object.entries(process.env)) {
         if (k.startsWith('INPUT_')) {
-            inputs[v.toLowerCase().substring(6)] = v;
+            inputs[k.toLowerCase().substring(6)] = v;
         }
     }
     return inputs;
